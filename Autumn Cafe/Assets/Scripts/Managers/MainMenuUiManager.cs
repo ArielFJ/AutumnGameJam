97d958c583bca53383aaf0fd65a2b029ee255160
ArielFJ <afermin266@gmail.com>
2021-09-17 00:16:44 -0400

    Main menu polish

--- Autumn Cafe/Assets/Scripts/Managers/MainMenuUiManager.cs
+++ Autumn Cafe/Assets/Scripts/Managers/MainMenuUiManager.cs
@@ -7,13 +7,14 @@ public class MainMenuUiManager : MonoBehaviour
 
     public void GoToOptions()
     {
-        _menuUI.SetActive(false);
+        //_menuUI.SetActive(false);
+        // TODO: use doTween to make it appear
         _optionsUI.SetActive(true);
     }
 
     public void GoToMainMenu()
     {
-        _menuUI.SetActive(true);
+        //_menuUI.SetActive(true);
         _optionsUI.SetActive(false);
     }
 }
